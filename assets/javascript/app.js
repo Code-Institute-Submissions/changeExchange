40e719183a6456c0f3178086568158f94e56ea1f
tod619 <trevorodwyer619@hotmail.com>
2020-05-07 17:07:03 +0100

    Add functionality for conversion in app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,14 @@
 //Variables
 
+// Variables for currency converter
+const currencyEL_one = document.getElementById("currencey-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEL_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
 // Typing effect class
 class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
@@ -75,7 +84,45 @@ document.querySelectorAll(".accordion-button").forEach((button) => {
   });
 });
 
+// functions for currency converter
+// fetch Exchange Rates and Update DOM
+function calculate() {
+  // Get the value of the select boxes
+  const currency_one = currencyEL_one.value;
+  const currency_two = currencyEL_two.value;
+
+  // Fetch from the exchange api by appending the select value onto the request
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    // Convert promise to json format
+    .then((res) => res.json())
+    .then((data) => {
+      //get the rate of currency two
+      const rate = data.rates[currency_two];
+      // Up date the rate text
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      // Calculate the amount which is the value of amount 1 * rate fixed to 2 decimal places
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
 // Action Listners
+
+swap.addEventListener("click", () => {
+  // Create a temp var to hold currency 1 value
+  const temp = currencyEL_one.value;
+  // Make curency 1 = to the value of currency 2
+  currencyEL_one.value = currencyEL_two.value;
+  // Make currency two value = to the value stored in temp(currencey 1)
+  currencyEL_two.value = temp;
+  calculate();
+});
+
+// Add EventListners for currency converter
+currencyEL_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEL_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
 
 // Init On DOM Load
 document.addEventListener("DOMContentLoaded", init);
@@ -88,6 +135,8 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+calculate();
 
 //Get the current year for the copyright at the bottom of the page
 $("#year").text(new Date().getFullYear());
