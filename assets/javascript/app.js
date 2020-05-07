98435ab33ae0297671bca103bc84cfe2168ddc1a
tod619 <trevorodwyer619@hotmail.com>
2020-05-07 18:30:09 +0100

    add toggle class in style + logic in app for when page is scrolled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -66,6 +66,14 @@ class TypeWriter {
 
 // Functions
 
+// Scroll effect for navbar
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#main-nav");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
 // Smooth Scrolling
 $("#main-nav a").on("click", function (event) {
   if (this.hash !== "") {
